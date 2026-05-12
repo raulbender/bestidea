9503b8df41fe05fbeebd39b5b9b3f536108b6d60
Raul Bender <196723837+raulbender@users.noreply.github.com>
2026-05-12 17:16:56 -0300

    feat(room): add author details to RoomDTO constructor

--- App/Components/Room/RoomDTO.php
+++ App/Components/Room/RoomDTO.php
@@ -9,6 +9,9 @@ class RoomDTO
     public function __construct(
         public ?string $uuid = null,
         public ?string $description = null,
-        public ?string $expires_at = null
+        public ?string $expires_at = null,
+        public ?string $author = null,
+        public ?string $avatar = null,
+        public ?int $author_id = null
     ) {}
 }
