5c36a98351059641b368156b635ab77bedbc468f
Raul Bender <196723837+raulbender@users.noreply.github.com>
2026-06-11 16:52:54 -0300

    feat(share-dropdown): add share dropdown component for copying room link

---
+++ App/Views/room/share_dropdown.php
@@ -0,0 +1,8 @@
+<div id="share-dropdown" class="drop-down-menu volt-animate">
+    
+    <a href="javascript:void(0)" onclick="VoltShare.copyToClipboard()">
+        <span>📋</span>
+        <span><?= __('rooms.copy_link')?></span>
+    </a>
+
+</div>
